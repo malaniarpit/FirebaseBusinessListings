444ba041ca2edca7705adc897a6d959d5a60b551
Arpit Malani <malaniarpit@gmail.com>
2017-02-04 07:15:00 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyBpPRKzWUnlA6dV7BAtlk8uQLKZaNQTYKU",
+    authDomain: "businesscontracts-c2002.firebaseapp.com",
+    databaseURL: "https://businesscontracts-c2002.firebaseio.com",
+    storageBucket: "businesscontracts-c2002.appspot.com",
+    messagingSenderId: "175729266929"
+  };
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
